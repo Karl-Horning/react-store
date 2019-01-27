e0d255e6342f8172d5842ae6df7dbd12ed2e717a
Karl Horning <k.horning@me.com>
2019-01-27 13:32:59 +0000

    Updated product list layout

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
 
 export default class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
   render() {
+    console.warn(this.state.products);
+
     return (
-      <div>
-        <div>
-          <Product />
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            {/* // Products Row */}
+            <Title name="our" title="products" />
+            <div className="row" />
+          </div>
         </div>
-      </div>
+      </React.Fragment>
+      // <Product />
     );
   }
 }
